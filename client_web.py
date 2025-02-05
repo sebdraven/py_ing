ea0efd562fd1498dbafff66df3d9b7c17ce74d2a
Sebastien Larinier <sebdraven@protonmail.com>
2025-02-05 10:52:02 +0100

    add argument for the script

--- client_web.py
+++ client_web.py
@@ -1,9 +1,21 @@
 import requests
+from argparse import ArgumentParser
 
-url = "https://jsonip.com/"
-r=requests.get(url)
-if r.status_code == requests.codes.ok:
-    print(f'{r.headers}')
-    print(f'{r.content}')
-    res = r.json()
-    print(f'Your IP is {res["ip"]}')
+def parse_args():
+    parser = ArgumentParser(description='Get your IP address')
+    parser.add_argument('-u', '--url', type=str, help='URL to get the IP address')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print the headers')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    r=None
+    if args.url:
+        r=requests.get(args.url)
+        if r.status_code == requests.codes.ok:
+            res = r.json()
+            print(f'Your IP is {res["ip"]}')
+    if args.verbose:
+        print(f'{r.headers}')
+        print(f'{r.content}')
+        
